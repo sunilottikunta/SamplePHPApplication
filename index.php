307ba6f3667d502478d03b7ee7f2797cf6570e01
sunilottikunta <sunilottikunta6@gmail.com>
2022-08-12 09:31:09 +0000

    Second Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html> 
+	<head> 
+	<title>PHP</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP 2022  Test</h1>
+		<p><b>Collins Session Day 5 -- Code Pipeline - Built from GIT </b></p>
+		<?php echo "The Current Date and yes las  Time is: <br />";
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information </h2> 
+		<p> <?php phpinfo(); ?> </p> 
+	</body> 
+</html>
+
